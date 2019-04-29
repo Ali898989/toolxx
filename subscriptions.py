20cf43f695dc6e89bc9205d5cfd862c86c09cf58
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-04-29 14:02:54 +0200

    Update subscriptions.py

--- subscriptions.py
+++ subscriptions.py
@@ -2,11 +2,15 @@ import random
 import os
 import time
 def hh():
- e = str(random.randint(1,1))
+ e = str(random.randint(1,2))
  print " [*]please wait... "
 
  if e in "1":
     os.system("termux-open https://www.youtube.com/channel/UCTlvUAypIKJ2BCUaUHKwrxg")
+ elif e in "2":
+
+    os.system("termux-open https://www.youtube.com/channel/UCAv0QRamd4d4UGL6Ibe3k7Q")
+   
  else :
     print "error"
  time.sleep(5)
