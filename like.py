cb955c523a958ff04ba4a563fc24c68891282d3a
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-04-29 22:08:17 +0200

    Update like.py

--- like.py
+++ like.py
@@ -2,7 +2,7 @@ import random
 import os
 import time
 def hh():
- e = str(random.randint(1,3))
+ e = str(random.randint(1,4))
 
  print " [*]please wait... "
  if e in "1":
@@ -11,6 +11,16 @@ def hh():
     os.system("termux-open https://youtu.be/QJCpdB5MOwE")
  elif e in "3":
     os.system("termux-open https://youtu.be/g05168s3tIw")
+ elif e in "4":
+   os.system("termux-open https://www.youtube.com/channel/UCAv0QRamd4d4UGL6Ibe3k7Q")
+
+
+   
+   
+   
+   
+   
+   
  else :
     print "error"
  time.sleep(5)
