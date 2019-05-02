938d02c07b755e17045fbaf8bcc78627a02c1a30
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-05-02 23:17:29 +0200

    Update show.py

--- show.py
+++ show.py
@@ -2,25 +2,32 @@ import random
 import os
 import time
 def hh():
- e = str(random.randint(1,8))
+ e = str(random.randint(1,7))
 
  print " [*]please wait... "
  if e in "1":
     os.system("termux-open https://youtu.be/dD8bjB8B0Jw")
+    time.sleep(100)
  elif e in "2":
     os.system("termux-open https://youtu.be/QJCpdB5MOwE")
+    time.sleep(290)
  elif e in "3":
     os.system("termux-open https://youtu.be/g05168s3tIw")
+    time.sleep(141)
  elif e in "4":
-   os.system("termux-open https://www.youtube.com/channel/UCAv0QRamd4d4UGL6Ibe3k7Q")
+   os.system("termux-open https://www.youtube.com/watch?v=kMf8xiJLPr8")
+   time.sleep(340)
+
  elif e in "5":
     os.system("termux-open https://youtu.be/coLoReiTx9g")
+    time.sleep(82)
  elif e in "6":
     os.system("termux-open https://youtu.be/FGWW0Wp-uTE")
+    time.sleep(82)
  elif e in "7":
-    os.system("termux-open https://youtu.be/z8_aV_tPB_8")
- elif e in "8":
     os.system("termux-open https://youtu.be/C8j8dIPuy6c")
+    time.sleep(88)
+
 
 
 
@@ -28,7 +35,7 @@ def hh():
 
  else :
     print "error"
- time.sleep(120)
+ #time.sleep(120)
  hh()
 
 hh()
