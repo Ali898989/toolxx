a7fa0e973c4bf2a61a9421f50d48446edcc06218
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-05-02 19:22:12 +0200

    Add files via upload

---
+++ show.py
@@ -0,0 +1,34 @@
+import random
+import os
+import time
+def hh():
+ e = str(random.randint(1,8))
+
+ print " [*]please wait... "
+ if e in "1":
+    os.system("termux-open https://youtu.be/dD8bjB8B0Jw")
+ elif e in "2":
+    os.system("termux-open https://youtu.be/QJCpdB5MOwE")
+ elif e in "3":
+    os.system("termux-open https://youtu.be/g05168s3tIw")
+ elif e in "4":
+   os.system("termux-open https://www.youtube.com/channel/UCAv0QRamd4d4UGL6Ibe3k7Q")
+ elif e in "5":
+    os.system("termux-open https://youtu.be/coLoReiTx9g")
+ elif e in "6":
+    os.system("termux-open https://youtu.be/FGWW0Wp-uTE")
+ elif e in "7":
+    os.system("termux-open https://youtu.be/z8_aV_tPB_8")
+ elif e in "8":
+    os.system("termux-open https://youtu.be/C8j8dIPuy6c")
+
+
+
+
+
+ else :
+    print "error"
+ time.sleep(120)
+ hh()
+
+hh()
